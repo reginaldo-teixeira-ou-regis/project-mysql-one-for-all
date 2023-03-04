442596294f7a0327f86dca762cc5c471c4a30d5d
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-03 23:13:05 -0300

    Challenge 2 Complete

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,4 +1,4 @@
 SELECT 
-	(SELECT COUNT(*) FROM musicas) AS "cancoes",
-  (SELECT COUNT(*) FROM artistas) AS "artistas",
-  (SELECT COUNT(*) FROM albuns) AS "albuns";
+	(SELECT COUNT(*) FROM musics) AS "cancoes",
+  (SELECT COUNT(*) FROM artists) AS "artistas",
+  (SELECT COUNT(*) FROM albums) AS "albuns";
