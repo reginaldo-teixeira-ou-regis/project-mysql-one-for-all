e037d9ba61afec71024e5c413f4fef2ca0f8aa61
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-03 23:16:29 -0300

    Challenge 3 Complete

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,9 +1,9 @@
 SELECT 
-    usr.nome_usuario AS "pessoa_usuaria", 
-    COUNT(hst.id_musica ) AS "musicas_ouvidas", 
-    ROUND(SUM(mus.duracao_musica)/60, 2) AS "total_minutos"
-  FROM usuario usr
-  JOIN historico hst ON usr.id_usuario = hst.id_usuario
-  JOIN musicas mus ON hst.id_musica = mus.id_musica
-  GROUP BY usr.nome_usuario
-  ORDER BY usr.nome_usuario;
+    usr.name_user AS "pessoa_usuaria", 
+    COUNT(hst.id_music ) AS "musicas_ouvidas", 
+    ROUND(SUM(mus.duration_music)/60, 2) AS "total_minutos"
+  FROM users usr
+  JOIN historic hst ON usr.id_user = hst.id_user
+  JOIN musics mus ON hst.id_music = mus.id_music
+  GROUP BY usr.name_user
+  ORDER BY usr.name_user;
