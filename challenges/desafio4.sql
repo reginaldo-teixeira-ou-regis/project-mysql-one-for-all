28fb886d9317d2494d7b2715df5922459c1afd48
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-04 00:57:15 -0300

    Challenge 4 Complete

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -1,6 +1,6 @@
 SELECT usr.name_user as pessoa_usuaria,
-    IF(YEAR(MAX(hst.reproduction_date_historic)) >= '2021', 'Ativa', 'Inativa')
-    AS status_pessoa_usuaria
+  IF(YEAR(MAX(hst.reproduction_date_historic)) >= '2021', 'Ativa', 'Inativa')
+  AS status_pessoa_usuaria
   FROM users usr
   JOIN historic hst ON hst.id_user = usr.id_user
   GROUP BY pessoa_usuaria
