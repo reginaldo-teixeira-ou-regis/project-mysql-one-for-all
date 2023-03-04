5784ceea79a7cd1c3dbd4f722a53fe5b9ab48827
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-04 00:24:34 -0300

    Challenge 7 Complete

---
+++ challenges/desafio7.sql
@@ -0,0 +1,9 @@
+SELECT 
+    artists.name_artist AS artista,
+    albums.name_album AS album,
+    COUNT(following_artists.id_user) AS pessoas_seguidoras
+  FROM albums
+  INNER JOIN artists ON artists.id_artist = albums.id_artist
+  INNER JOIN following_artists ON following_artists.id_artist = artists.id_artist
+  GROUP BY artista, album 
+  ORDER BY pessoas_seguidoras DESC, artista, album;
