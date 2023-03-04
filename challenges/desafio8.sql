48ecd6a9282e10853aa795ca059dbdcf3cf9dd63
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-04 00:28:15 -0300

    Challenge 8 Complete

---
+++ challenges/desafio8.sql
@@ -0,0 +1,7 @@
+SELECT 
+    artists.name_artist AS artista,
+    albums.name_album AS album
+  FROM artists
+  INNER JOIN albums ON albums.id_artist = artists.id_artist
+  WHERE artists.name_artist = 'Elis Regina'
+  ORDER BY album;
